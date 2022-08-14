476b0bfb8b054ce84c211aa28fc2d2b6dc02d745
DavGuett <soulsec@protonmail.com>
2022-08-14 20:39:10 -0300

    Primeiros Refinamentos

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const menuTarefa = document.querySelector('.tarefas-container');
 const input = document.querySelector('.campo-tarefa');
+const button = document.querySelector('.btn');
+button.addEventListener('click', addTarefa);
+
 
 function addTarefa() {
-  const tarefa = document.createElement('div');
-  tarefa.innerHTML = `<input type="checkbox" name="check" id="check"></input><p>${input.value}</p>`;
-  menuTarefa.appendChild(tarefa);
+  if(!input.value == '') {
+    const tarefa = document.createElement('div');
+    tarefa.classList.add('tarefa');
+    tarefa.innerHTML = `<input type="checkbox" name="check" id="check"></input><p>${input.value}</p>`;
+    menuTarefa.appendChild(tarefa);
+  }
+  
 }
